0b38756b3dbe75c82c9e1b6ac215195d679f2d1f
janekim0730 <vagrant@scotchbox>
2016-05-12 02:07:24 +0000

    initialized

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+require('./styles/main.scss');
+require('./styles/reset.scss');
+
+class App extends React.Component {
+  render(){
+    return
+  }
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById('react-app'));
